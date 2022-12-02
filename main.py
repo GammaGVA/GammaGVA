2127aeb74f19076422486ce82476456f5d35126c
GammaGVA <gri_v_a@vk.com>
2022-12-02 16:47:30 +0300

    Выяснил что у avito особый "талант" усложняться. Они выводят не больше 100 страниц с объявлениями. Похожие примерно, в основном различие этаж и цена(если это застройщик), группируют и делают ссылку в перечне объявлений, на самом объявлении. Позже сделаю цикл while и условие all_pages != count. Заведу count, будет считать сколько раз цикл повторился. И надо будет попробовать тот дешёвый прокси сервер, чтоб сразу несколько фильтров одновременно parser-ить. И надо поискать библиотеке с хорошим функционалом для записи в csv файл. В идеале чтоб делалось несколько файлов по застройщикам, в них несколько страниц по кол-вам комнат.

--- main.py
+++ main.py
@@ -1,149 +1,67 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from easyocr import Reader
-import csv
-from os import remove
+from search_all_info import search_info_apartment_house
 
 options = webdriver.ChromeOptions()
 options.add_argument(
     "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.headless = True
+# options.headless = True
 # Для теневой работы добавил - options.headless = True
 driver = webdriver.Chrome(
     service=Service("/home/ivan/PycharmProjects/pythonProject/GammaGVA/chromedriver"),
     options=options)
 
-flag_write_headrs = True
+flag_write_headers = True
 # Для будущего if-а записи в csv
 
-driver.get(
-    url="https://www.avito.ru/voronezh/kvartiry/prodam/1-komnatnye/novostroyka-ASgBAQICAkSSA8YQ5geOUgFAyggUgFk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA")
+url = "https://www.avito.ru/voronezh/kvartiry/prodam/1-komnatnye/novostroyka-ASgBAQICAkSSA8YQ5geOUgFAyggUgFk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA"
+
+driver.get(url=url)
 all_pages = int(driver.find_elements(by='class name', value='pagination-item-JJq_j')[-2].text)
 
-with open('apartments.csv', 'a') as f_csv:
-    for _ in range(all_pages - 1):
-        all_links = driver.find_elements(by='class name', value='iva-item-content-rejJg')
-        count_links = len(all_links)
+for _ in range(all_pages - 1):
+    all_links = driver.find_elements(by='class name', value='iva-item-content-rejJg')
+    count_links = len(all_links)
 
-        for link in range(1, count_links + 1):
-            try:
-                driver.find_element(by='xpath', value=f'/html/body/div[1]/div/div[3]/div[3]/div[3]/div[5]/div[2]/div[{link}]/div/div[3]/div/div[1]/div[1]/a')
-                # Этим делаю отбор только застройщиков.
-            except:
-                continue
+    for link in all_links:
+        try:
+            search_info_apartment_house(link=link, driver=driver, flag_write_headers=flag_write_headers)
+        except:
+            continue
+        # Пока оставлю так, позже добавлю исключения.
+        # Решил parser-ить только застройщиков.
+        # Ищем лого, в функции добавлю ещё поиск "застройщик"
+        driver.implicitly_wait(2)
+        driver.close()
+        driver.implicitly_wait(2)
+        driver.switch_to.window(driver.window_handles[0])
 
-            url_link = driver.find_element(by='xpath',value=f'/html/body/div[1]/div/div[3]/div[3]/div[3]/div[5]/div[2]/div[{link}]/div/div[2]/div[2]/a').get_attribute('href')
+        flag_write_headers = False
+        # Внутри функции не поменяешь флаг, а шапку записывать до не комильфо.
 
-            driver.find_element(by='xpath', value=f'/html/body/div[1]/div/div[3]/div[3]/div[3]/div[5]/div[2]/div[{link}]/div/div[2]/div[2]/a').click()
-            # Абсолютно тупой способ, но работает. Пока не понял как из уже найденного all_links дальше найти.
-            # Пробовал по-разному, но он мне 50 одинаковых ссылок выдавал.
-            # Надо что-то с этим делать, а то лишние запросы провожу тогда.
-            # try except справляется, и вроде как они работают быстрее чем if, но это не дело.
-            driver.implicitly_wait(2)
-            driver.switch_to.window(driver.window_handles[1])
-
-            neme_devoloper = driver.find_element(by='class name', value='text-size-ms-_Zk4a').text
-            price = driver.find_element(by='class name', value='text-size-xxl-UPhmI').text.strip()
-            description = driver.find_element(by='class name', value='style-item-description-html-qCwUL').text.strip().replace('\n', ' ')
-            address = driver.find_element(by='class name', value='style-item-address-KooqC').text.strip().replace('\n', ' ')
-            price_metr2 = driver.find_element(by='class name', value='style-item-price-sub-price-_5RUD').text.strip()
-
-            driver.find_element(by='xpath', value='/html/body/div[1]/div/div[2]/div/div[3]/div[4]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/span/span/div/div/button').click()
-            # driver.find_element(by='xpath', value='//div[@data-marker="item-phone-button/card"]').click()
-            driver.implicitly_wait(2)
-            link_phone_img = driver.find_element(by='class name', value='item-popup-phoneImage-adVhz').get_attribute('src')
-            driver.get(url=link_phone_img)
-            driver.implicitly_wait(2)
-            driver.save_screenshot(filename='/home/ivan/PycharmProjects/pythonProject/phone.png')
-            driver.back()
-            # Я так и не нашёл способа без скачивания, распознать текст.
-            # Так же информация как скачать из seleniuma прошла мимо
-            # Теперь вот скришотом.
-            # Можно было бы не переходить на ссылку картинки и так скриншот сделать. Но не хотелесь с координатами скриншота возиться.
-
-            phone: list = Reader(['ru']).readtext('/home/ivan/PycharmProjects/pythonProject/phone.png', detail=0)
-
-            # Не видит карту при установленном cuda, и process finished with exit code 132 (interrupted by signal 4 sigill) при распозновании
-            # Решение пока не нашёл, на форумах написано, такое при не соответствии архитектуры - устранил.
-
-            all_info_about_the_apartment = driver.find_elements(by='class name', value='params-paramsList__item-appQw')
-            info_dict_apartment_house = {'url': url_link,
-                                         'Застройщик': neme_devoloper,
-                                         'Цена': price,
-                                         'Цена за метр': price_metr2,
-                                         'Адрес': address,
-                                         'Телефон': ' '.join(phone),
-                                         'Количество комнат': 'Данных не представлялись',
-                                         'Общая площадь': 'Данные не предоставлены',
-                                         'Площадь кухни': 'Данные не предоставлены',
-                                         'Жилая площадь': 'Данные не предоставлены',
-                                         'Этаж': 'Данные не предоставлены',
-                                         'Балкон или лоджия': 'Данные не предоставлены',
-                                         'Высота потолков': 'Данные не предоставлены',
-                                         'Санузел': 'Данные не предоставлены',
-                                         'Окна': 'Данные не предоставлены',
-                                         'Отделка': 'Данные не предоставлены',
-                                         'Тип комнат': 'Данные не предоставлены',
-                                         'Название новостройки': 'Данные не предоставлены',
-                                         'Корпус, строение': 'Данные не предоставлены',
-                                         'Официальный застройщик': 'Данные не предоставлены',
-                                         'Тип участия': 'Данные не предоставлены',
-                                         'Срок сдачи': 'Данные не предоставлены',
-                                         'Тип дома': 'Данные не предоставлены',
-                                         'Этажей в доме': 'Данные не предоставлены',
-                                         'Пассажирский лифт': 'Данные не предоставлены',
-                                         'Грузовой лифт': 'Данные не предоставлены',
-                                         'Двор': 'Данные не предоставлены',
-                                         'Парковка': 'Данные не предоставлены',
-                                         'Способ продажи': 'Данные не предоставлены',
-                                         'Описание': description}
-            # А может сделать тогда один большой словарь, квартира и дом.
-            # Да и сразу все данные найденные ранее воткнуть сюда. Как раз сгрупирую всё как положено.
-            # Это должны быть все варианты, теперь только сгрупировать.
-            # Описание пока пхнул в конец, не разобрался как обзац делать при записи, \n в строке не конает.
-
-            for point_info in all_info_about_the_apartment:
-                lst_point = point_info.text.split(':')
-                info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
-
-            all_info_the_house = driver.find_elements(by='class name', value='style-item-params-list-item-aXXql')
-            for point_info in all_info_the_house:
-                lst_point = point_info.text.split(':')
-                info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
-
-            remove('/home/ivan/PycharmProjects/pythonProject/phone.png')
-            # Удаляем скриншот.
-
-            keys = []
-            values = []
-
-            for key, value in info_dict_apartment_house.items():
-                keys.append(key)
-                values.append(value)
-
-            # Начал думать с записью в csv и встрял. Все словари разные.
-            # Разное количество столбцов, да и встречаются разные типы данных.
-            # Где-то указан потолок, но не отделки, где-то отделка, но нет этажа, или тиа планировки.
-            # Информации по поводу записи по названиям столбцов в csv не нашёл. Попробую ещё поискать.
-            # Как обходное решение, думаю создать словарь, с важными категориями и значением None.
-            # А уже при парсинге забивать значения, туда где они есть.
-
-            writer = csv.writer(f_csv)
-
-            if flag_write_headrs:
-                writer.writerow(keys)
-                flag_write_headrs = False
-
-            writer.writerow(values)
-
-            driver.implicitly_wait(2)
-            driver.close()
-            driver.implicitly_wait(2)
-            driver.switch_to.window(driver.window_handles[0])
-
-        driver.find_elements(by='class name', value='pagination-item-JJq_j')[-1].click()
-    # датамаркер pagination-button/next
+    driver.find_elements(by='class name', value='pagination-item-JJq_j')[-1].click()
+    """
+    У avito фишка такая, они не показывают сколько страниц на самом деле с товаром.
+    Надо думать, как находить число страниц.
+    Есть вариант в цикл while засунуть, но перед этим найти общее число объявлений и отнимать кол-во ссылок на странице.
+    Поставить условие all_ads > 0 и так вот нажимать в цикле - data-marker="pagination-button/next"
+    Хотя вот сука когда как, у меня parser нашёл 72 странице, и в браузере столько вижу.
+    При переходе на 72 их становится 75.
+    Но делаю другой поиск, там 100 страниц, при переходе на 100 их также и осталось.
+    Я не понимаю avito, они максимум выводят 5000 объявлений, хотя пишут и 10 000 и 8 000 и 18 000.
+    Разобрался, они выводят 100 страниц всегда, искал и по другим городам. Просто группируют похожие объявления,
+    если объявлений больше 5к. так что, для пущей статистики, надо либо переходить по ссылки,
+    либо просто фиксировать кол-во похожих.
+    Надо будет переходить по ним, там цена немного разнится.
+    А если parser-ить частников, то предложит сравнить 2-х продавцов,
+    и не понятно, будет ли дальше его объявление или нет.
+    Решил на частников забить, там и логика меняется, проверены не проверены, собственник не собственник и т.д.
+    Как и задумывал, буду parser новостройки, и наверное только от застройщиков.
+    Логи в новостройках не меняется, у разных объявлений всё одно и тоже, разного нет.
+    parser-ить по новостройкам, с расчётом на то что выведет 100 страниц. Просто придумать условие.
+    Надо будет сбор информации в функцию запихнуть, а то при переходе на страницу "похожие объявления шт.
+    """
 
 driver.close()
 driver.quit()
