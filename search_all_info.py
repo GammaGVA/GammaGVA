56f922e5235a3cbf765e3693ee62138a9dc8be45
GammaGVA <gri_v_a@vk.com>
2022-12-05 15:20:30 +0300

    Проэксперементировал с определением телефона. Убрал запрос ссылки на картинку, теперь делаю скрин сразу. Изменил запись, обхожусь без циклов и флага.

--- search_all_info.py
+++ search_all_info.py
@@ -1,92 +1,95 @@
 from easyocr import Reader
 import csv
-from os import remove
+from PIL import Image
+from time import sleep
 
 
-def search_info_apartment_house(link, driver, flag_write_headers):
+def search_info_apartment_house(driver, flag_write_headers):
+    try:
+        url_link = driver.current_url
+        trader = driver.find_element(by='xpath', value='//div[@data-marker="seller-info/label"]').text
+        name_developer = driver.find_element(by='class name', value='text-size-ms-_Zk4a').text
+        price = driver.find_element(by='class name', value='text-size-xxl-UPhmI').text.strip()
+        address = driver.find_element(by='class name', value='style-item-address-KooqC').text.strip().replace('\n', ' ')
+        price_metr2 = driver.find_element(by='class name', value='style-item-price-sub-price-_5RUD').text.strip()
 
-    link.find_element(by='xpath', value=f'//div[@data-marker="item-user-logo"]')
+        info_dict_apartment_house = {'url': url_link,
+                                     'Застройщик': name_developer,
+                                     'Цена': price,
+                                     'Продавец': trader,
+                                     'Цена за метр': price_metr2,
+                                     'Адрес': address,
+                                     'Телефон': '-',
+                                     'Количество комнат': '-',
+                                     'Общая площадь': '-',
+                                     'Площадь кухни': '-',
+                                     'Жилая площадь': '-',
+                                     'Этаж': '-',
+                                     'Балкон или лоджия': '-',
+                                     'Высота потолков': '-',
+                                     'Санузел': '-',
+                                     'Окна': '-',
+                                     'Отделка': '-',
+                                     'Тип комнат': '-',
+                                     'Название новостройки': '-',
+                                     'Корпус, строение': '-',
+                                     'Официальный застройщик': '-',
+                                     'Тип участия': '-',
+                                     'Срок сдачи': '-',
+                                     'Тип дома': '-',
+                                     'Этажей в доме': '-',
+                                     'Пассажирский лифт': '-',
+                                     'Грузовой лифт': '-',
+                                     'Двор': '-',
+                                     'Парковка': '-',
+                                     'Способ продажи': '-'}
 
-    url_link = link.find_element(by='xpath', value=f'//div/div[2]/div[2]/a').get_attribute('href')
+        driver.find_element(by='xpath', value='//button[@data-marker="item-phone-button/card"]').click()
+        driver.implicitly_wait(15)
 
-    link.find_element(by='class name', value=f'iva-item-titleStep-pdebR').click()
-    driver.implicitly_wait(2)
-    driver.switch_to.window(driver.window_handles[1])
+        sleep(2)
+        # Добавил sleep, так как порой делает скрин ещё не прогруженной страницы.
 
-    name_developer = driver.find_element(by='class name', value='text-size-ms-_Zk4a').text
-    price = driver.find_element(by='class name', value='text-size-xxl-UPhmI').text.strip()
-    description = driver.find_element(by='class name', value='style-item-description-html-qCwUL').text.strip().replace(
-        '\n', ' ')
-    address = driver.find_element(by='class name', value='style-item-address-KooqC').text.strip().replace('\n', ' ')
-    price_metr2 = driver.find_element(by='class name', value='style-item-price-sub-price-_5RUD').text.strip()
+        driver.save_screenshot(filename='/home/ivan/PycharmProjects/pythonProject/phone.png')
+        x = 90
+        y = 70
+        w = 470
+        h = 150
+        # Пришлось делать скрин сразу. Но при автоопределении координат, резалось неправильно. Пришлось подгонять самому.
+        # А скрин не тех размеров, и с авто координатами обрезает неверно.
+        # Selenium при работе в фоновом режиме, работает в разрешении 800-600.
+        fullImg = Image.open("/home/ivan/PycharmProjects/pythonProject/phone.png")
+        cropImg = fullImg.crop((x, y, w, h))
+        cropImg.save('/home/ivan/PycharmProjects/pythonProject/phone1.png')
 
-    driver.find_element(by='xpath', value='//button[@data-marker="item-phone-button/card"]').click()
-    driver.implicitly_wait(2)
+        no_phone = ' '.join(Reader(['ru']).readtext('/home/ivan/PycharmProjects/pythonProject/phone1.png', detail=0))
+        phone = ''
+        for _ in no_phone:
+            if _.isdigit():
+                phone += _
+        # Пришлось срезы делать с запасом, и проходить этот цикл, чтоб только цифры взять.
 
-    link_phone_img = driver.find_element(by='class name', value='item-popup-phoneImage-adVhz').get_attribute('src')
+        info_dict_apartment_house['Телефон'] = phone
+        # Надо было это сделать, на страницах разные крестики могут быть, и адреса у них разные. Схожего элемента нет.
 
-    driver.get(url=link_phone_img)
-    driver.implicitly_wait(2)
-    driver.save_screenshot(filename='/home/ivan/PycharmProjects/pythonProject/phone.png')
-    driver.back()
+        all_info_about_the_apartment = driver.find_elements(by='class name', value='params-paramsList__item-appQw')
+        for point_info in all_info_about_the_apartment:
+            lst_point = point_info.text.split(':')
+            info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
 
-    phone: list = Reader(['ru']).readtext('/home/ivan/PycharmProjects/pythonProject/phone.png', detail=0)
+        all_info_the_house = driver.find_elements(by='class name', value='style-item-params-list-item-aXXql')
+        for point_info in all_info_the_house:
+            lst_point = point_info.text.split(':')
+            info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
 
-    all_info_about_the_apartment = driver.find_elements(by='class name', value='params-paramsList__item-appQw')
-    info_dict_apartment_house = {'url': url_link,
-                                 'Застройщик': name_developer,
-                                 'Цена': price,
-                                 'Цена за метр': price_metr2,
-                                 'Адрес': address,
-                                 'Телефон': ' '.join(phone),
-                                 'Количество комнат': '-',
-                                 'Общая площадь': '-',
-                                 'Площадь кухни': '-',
-                                 'Жилая площадь': '-',
-                                 'Этаж': '-',
-                                 'Балкон или лоджия': '-',
-                                 'Высота потолков': '-',
-                                 'Санузел': '-',
-                                 'Окна': '-',
-                                 'Отделка': '-',
-                                 'Тип комнат': '-',
-                                 'Название новостройки': '-',
-                                 'Корпус, строение': '-',
-                                 'Официальный застройщик': '-',
-                                 'Тип участия': '-',
-                                 'Срок сдачи': '-',
-                                 'Тип дома': '-',
-                                 'Этажей в доме': '-',
-                                 'Пассажирский лифт': '-',
-                                 'Грузовой лифт': '-',
-                                 'Двор': '-',
-                                 'Парковка': '-',
-                                 'Способ продажи': '-',
-                                 'Описание': description}
+        with open('apartments.csv', 'a') as f_csv:
+            writer = csv.DictWriter(f_csv, fieldnames=info_dict_apartment_house.keys())
 
-    for point_info in all_info_about_the_apartment:
-        lst_point = point_info.text.split(':')
-        info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
+            if flag_write_headers:
+                writer.writeheader()
 
-    all_info_the_house = driver.find_elements(by='class name', value='style-item-params-list-item-aXXql')
-    for point_info in all_info_the_house:
-        lst_point = point_info.text.split(':')
-        info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
-
-    remove('/home/ivan/PycharmProjects/pythonProject/phone.png')
-    # Удаляем скриншот.
-
-    keys = []
-    values = []
-
-    for key, value in info_dict_apartment_house.items():
-        keys.append(key)
-        values.append(value)
-
-    with open('apartment.csv', 'a') as f_csv:
-        writer = csv.writer(f_csv)
-
-        if flag_write_headers:
-            writer.writerow(keys)
-
-        writer.writerow(values)
+            writer.writerow(info_dict_apartment_house)
+    except:
+        url_link = driver.current_url
+        print(url_link)
+        # сделал для контроля, если всё упадёт на поиске элемента, что б понять, есть этот элемент или не догрузилось.
