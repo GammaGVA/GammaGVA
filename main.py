99c5d9ccde69f9655cd39accf149d0a58a86dab6
GammaGVA <gri_v_a@vk.com>
2022-12-06 18:16:30 +0300

    Начал подискивать элементы через суп. Решил уйти от обрезания скриншота и просто скринить страницу только с номером.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from search_all_info import search_info_apartment_house
+from bs4 import BeautifulSoup
+import time
 
 options = webdriver.ChromeOptions()
 options.add_argument(
     "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
 options.add_argument("--disable-blink-features=AutomationControlled")
-# options.add_argument("--start-maximized")
-# Хотел полноэкранный режим, но при фоновой работе он не работает. Так что координаты среза скриншота подгоняем.
-options.headless = True
+# options.headless = True
 driver = webdriver.Chrome(
     service=Service("/home/ivan/PycharmProjects/pythonProject/GammaGVA/chromedriver"),
     options=options)
@@ -18,15 +18,14 @@ flag_write_headers = True
 
 url = "https://www.avito.ru/voronezh/kvartiry/prodam/novostroyka-ASgBAQICAUSSA8YQAUDmBxSOUg?f=ASgBAQICAUSSA8YQAkDmBxSOUpC~DRSSrjU"
 driver.get(url=f'{url}&p=100')
-all_pages = int(driver.find_elements(by='class name', value='pagination-item-JJq_j')[-2].text)
-
+soup = BeautifulSoup(driver.page_source, 'lxml')
+all_pages = int(soup.find_all('span', class_='pagination-item-JJq_j')[-2].text)
 driver.get(url=f'{url}&p=1')
-# Это было сделано, что бы узнать настоящее кол-во страниц с товаром.
-# Думал через while и if сделать, но решил так. Никогда не помешает узнать настоящее кол-во страниц.
+# Перехожу на 100 страницу сразу, что бы узнать кол-во страниц. С супом быстрее.
 
 for _ in range(all_pages - 1):
     all_links = driver.find_elements(by='class name', value='iva-item-content-rejJg')
-
+    alls = time.time()
     for link in all_links:
         try:
             link.find_element(by='class name', value='iva-item-groupingsStep-T_3Y7').click()
@@ -35,6 +34,7 @@ for _ in range(all_pages - 1):
 
             try:
                 pod_all_page = int(driver.find_elements(by='class name', value='pagination-item-JJq_j')[-2].text)
+                # Просто не бывает меньше двух
             except:
                 pod_all_page = 2
 
@@ -47,7 +47,9 @@ for _ in range(all_pages - 1):
                     driver.implicitly_wait(2)
                     driver.switch_to.window(driver.window_handles[2])
 
+                    start = time.time()
                     search_info_apartment_house(driver=driver, flag_write_headers=flag_write_headers)
+                    print(time.time()-start)
 
                     driver.close()
                     driver.switch_to.window(driver.window_handles[1])
@@ -69,16 +71,18 @@ for _ in range(all_pages - 1):
             # Да и фильтрация по лого так себе. Оказались застройщики без лого. И много агенств с лого.
 
             link.find_element(by='class name', value=f'iva-item-titleStep-pdebR').click()
-            driver.implicitly_wait(2)
             driver.switch_to.window(driver.window_handles[1])
 
+            start = time.time()
             search_info_apartment_house(driver=driver, flag_write_headers=flag_write_headers)
+            print(time.time() - start)
 
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
 
         flag_write_headers = False
         # Внутри функции не поменяешь флаг, а шапку записывать до не комильфо.
+    print(f'!!!{time.time()-alls}!!!')
 
     driver.find_element(by='xpath', value='//span[@data-marker="pagination-button/next"]').click()
 
--- search_all_info.py
+++ search_all_info.py
@@ -1,95 +1,88 @@
 from easyocr import Reader
 import csv
-from PIL import Image
 from time import sleep
+from bs4 import BeautifulSoup
 
-
-def search_info_apartment_house(driver, flag_write_headers):
+def _info(soup):
     try:
-        url_link = driver.current_url
-        trader = driver.find_element(by='xpath', value='//div[@data-marker="seller-info/label"]').text
-        name_developer = driver.find_element(by='class name', value='text-size-ms-_Zk4a').text
-        price = driver.find_element(by='class name', value='text-size-xxl-UPhmI').text.strip()
-        address = driver.find_element(by='class name', value='style-item-address-KooqC').text.strip().replace('\n', ' ')
-        price_metr2 = driver.find_element(by='class name', value='style-item-price-sub-price-_5RUD').text.strip()
-
-        info_dict_apartment_house = {'url': url_link,
-                                     'Застройщик': name_developer,
-                                     'Цена': price,
-                                     'Продавец': trader,
-                                     'Цена за метр': price_metr2,
-                                     'Адрес': address,
-                                     'Телефон': '-',
-                                     'Количество комнат': '-',
-                                     'Общая площадь': '-',
-                                     'Площадь кухни': '-',
-                                     'Жилая площадь': '-',
-                                     'Этаж': '-',
-                                     'Балкон или лоджия': '-',
-                                     'Высота потолков': '-',
-                                     'Санузел': '-',
-                                     'Окна': '-',
-                                     'Отделка': '-',
-                                     'Тип комнат': '-',
-                                     'Название новостройки': '-',
-                                     'Корпус, строение': '-',
-                                     'Официальный застройщик': '-',
-                                     'Тип участия': '-',
-                                     'Срок сдачи': '-',
-                                     'Тип дома': '-',
-                                     'Этажей в доме': '-',
-                                     'Пассажирский лифт': '-',
-                                     'Грузовой лифт': '-',
-                                     'Двор': '-',
-                                     'Парковка': '-',
-                                     'Способ продажи': '-'}
-
-        all_info_about_the_apartment = driver.find_elements(by='xpath', value='//li[@class="params-paramsList__item-appQw"]')
-        for point_info in all_info_about_the_apartment:
-            lst_point = point_info.text.split(':')
-            info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
-
-        all_info_the_house = driver.find_elements(by='xpath', value='//li[@class="style-item-params-list-item-aXXql"]')
-        for point_info in all_info_the_house:
-            lst_point = point_info.text.split(':')
-            info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip()
-
-        driver.find_element(by='xpath', value='//button[@data-marker="item-phone-button/card"]').click()
-        driver.implicitly_wait(15)
-
-        sleep(2)
-        # Добавил sleep, так как порой делает скрин ещё не прогруженной страницы.
-
-        driver.save_screenshot(filename='/home/ivan/PycharmProjects/pythonProject/phone.png')
-        x = 90
-        y = 70
-        w = 470
-        h = 150
-        # Пришлось делать скрин сразу. Но при автоопределении координат, резалось неправильно. Пришлось подгонять самому.
-        # А скрин не тех размеров, и с авто координатами обрезает неверно.
-        # Selenium при работе в фоновом режиме, работает в разрешении 800-600.
-        fullImg = Image.open("/home/ivan/PycharmProjects/pythonProject/phone.png")
-        cropImg = fullImg.crop((x, y, w, h))
-        cropImg.save('/home/ivan/PycharmProjects/pythonProject/phone1.png')
-
-        no_phone = ' '.join(Reader(['ru']).readtext('/home/ivan/PycharmProjects/pythonProject/phone1.png', detail=0))
-        phone = ''
-        for _ in no_phone:
-            if _.isdigit():
-                phone += _
-        # Пришлось срезы делать с запасом, и проходить этот цикл, чтоб только цифры взять.
-
-        info_dict_apartment_house['Телефон'] = phone
-        # Надо было это сделать, на страницах разные крестики могут быть, и адреса у них разные. Схожего элемента нет.
-
-        with open('apartments.csv', 'a') as f_csv:
-            writer = csv.DictWriter(f_csv, fieldnames=info_dict_apartment_house.keys())
-
-            if flag_write_headers:
-                writer.writeheader()
-
-            writer.writerow(info_dict_apartment_house)
+        trader = soup.find('div', {'data-marker': 'seller-info/label'}).text
+        name_developer = soup.find('span', class_='text-size-ms-_Zk4a').text
+        price = soup.find('span', class_='text-size-xxl-UPhmI').text
+        address = soup.find('div', class_='style-item-address-KooqC').text.strip().replace('\n', ' ')
+        price_metr2 = soup.find('div', class_='style-item-price-sub-price-_5RUD').text
+        return trader, name_developer, price, address, price_metr2
     except:
-        url_link = driver.current_url
-        print(url_link)
-        # сделал для контроля, если всё упадёт на поиске элемента, что б понять, есть этот элемент или не догрузилось.
+        print('Ошибка получения информации.')
+        return _info(soup)
+
+def _info_phone(soup, driver):
+    try:
+        sleep(1)
+        link_phone = soup.find('img', class_='item-popup-phoneImage-adVhz').get('src')
+        driver.get(url=link_phone)
+        driver.save_screenshot(filename='/home/ivan/PycharmProjects/pythonProject/phone.png')
+        driver.back()
+        phone = ' '.join(Reader(['ru']).readtext('/home/ivan/PycharmProjects/pythonProject/phone.png', detail=0))
+        return phone
+    except:
+        print('Ошибка получения телефона.')
+        return _info_phone(soup, driver)
+def search_info_apartment_house(driver, flag_write_headers):
+    url_link = driver.current_url
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    trader, name_developer, price, address, price_metr2 = _info(soup)
+    info_dict_apartment_house = {'url': url_link,
+                                 'Застройщик': name_developer,
+                                 'Цена': price.replace('\xa0', ' '),
+                                 'Продавец': trader,
+                                 'Цена за метр': price_metr2.replace('\xa0', ' '),
+                                 'Адрес': address,
+                                 'Телефон': '-',
+                                 'Количество комнат': '-',
+                                 'Общая площадь': '-',
+                                 'Площадь кухни': '-',
+                                 'Жилая площадь': '-',
+                                 'Этаж': '-',
+                                 'Балкон или лоджия': '-',
+                                 'Высота потолков': '-',
+                                 'Санузел': '-',
+                                 'Окна': '-',
+                                 'Отделка': '-',
+                                 'Тип комнат': '-',
+                                 'Название новостройки': '-',
+                                 'Корпус, строение': '-',
+                                 'Официальный застройщик': '-',
+                                 'Тип участия': '-',
+                                 'Срок сдачи': '-',
+                                 'Тип дома': '-',
+                                 'Этажей в доме': '-',
+                                 'Пассажирский лифт': '-',
+                                 'Грузовой лифт': '-',
+                                 'Двор': '-',
+                                 'Парковка': '-',
+                                 'Способ продажи': '-'}
+
+    all_info_about_the_apartment = soup.find_all('li', class_='params-paramsList__item-appQw')
+    for point_info in all_info_about_the_apartment:
+        lst_point = point_info.text.split(':')
+        info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip().replace('\xa0', ' ')
+
+    all_info_the_house = soup.find_all('li', class_='style-item-params-list-item-aXXql')
+    for point_info in all_info_the_house:
+        lst_point = point_info.text.split(':')
+        info_dict_apartment_house[lst_point[0].strip()] = lst_point[1].strip().replace('\xa0', ' ')
+
+    driver.find_element(by='xpath', value='//button[@data-marker="item-phone-button/card"]').click()
+    sleep(1)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    info_dict_apartment_house['Телефон'] = _info_phone(soup, driver)
+
+    with open('apartments.csv', 'a') as f_csv:
+        writer = csv.DictWriter(f_csv, fieldnames=info_dict_apartment_house.keys())
+
+        if flag_write_headers:
+            writer.writeheader()
+
+        writer.writerow(info_dict_apartment_house)
