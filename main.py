676d6a5babb08d3199a1153b769d40e05e0162ed
GammaGVA <gri_v_a@vk.com>
2022-12-08 16:27:21 +0300

    Переделал логику. Меньше селениума. Дальше буду думать о потоках и многопроцессорности. Часть информации получаю через респонс, в частности ссылки на объявления. В одном процессе 5 минут собирает сбор 1000 ссылок. Записываю их в файл. Заметил, что если в ссылку подставлять номер страницы то объявления могут повторяться. Не боюсь что будет повтор, для этого сет есть, опасаюсь что часть ссылок не доберу. Так что решил это прокликать по нексту. Добавить бы прокликивание с конца в начало. Можно будет это на два процесса разбить.

--- main.py
+++ main.py
@@ -1,90 +1,104 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from resp import number_max_page, reg_pod_links, soup_links
 from search_all_info import search_info_apartment_house
-from bs4 import BeautifulSoup
 import time
+import csv
 
-options = webdriver.ChromeOptions()
-options.add_argument(
-    "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
-options.add_argument("--disable-blink-features=AutomationControlled")
-# options.headless = True
-driver = webdriver.Chrome(
-    service=Service("/home/ivan/PycharmProjects/pythonProject/GammaGVA/chromedriver"),
-    options=options)
 
-flag_write_headers = True
-# Для будущего if-а записи в csv
+def search_links():
+    start = time.time()
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.headless = True
+    driver = webdriver.Chrome(
+        service=Service("/home/ivan/PycharmProjects/pythonProject/chromedriver"),
+        options=options)
 
-url = "https://www.avito.ru/voronezh/kvartiry/prodam/novostroyka-ASgBAQICAUSSA8YQAUDmBxSOUg?f=ASgBAQICAUSSA8YQAkDmBxSOUpC~DRSSrjU"
-driver.get(url=f'{url}&p=100')
-soup = BeautifulSoup(driver.page_source, 'lxml')
-all_pages = int(soup.find_all('span', class_='pagination-item-JJq_j')[-2].text)
-driver.get(url=f'{url}&p=1')
-# Перехожу на 100 страницу сразу, что бы узнать кол-во страниц. С супом быстрее.
+    cookie = 'Возьми у себя из браузера'.split(';')
 
-for _ in range(all_pages - 1):
-    all_links = driver.find_elements(by='class name', value='iva-item-content-rejJg')
-    alls = time.time()
-    for link in all_links:
-        try:
-            link.find_element(by='class name', value='iva-item-groupingsStep-T_3Y7').click()
-            driver.implicitly_wait(2)
-            driver.switch_to.window(driver.window_handles[1])
+    url = "https://www.avito.ru/voronezh/kvartiry/prodam/novostroyka-ASgBAQICAUSSA8YQAUDmBxSOUg?f=ASgBAQICAUSSA8YQAkDmBxSOUpC~DRSSrjU"
 
-            try:
-                pod_all_page = int(driver.find_elements(by='class name', value='pagination-item-JJq_j')[-2].text)
-                # Просто не бывает меньше двух
-            except:
-                pod_all_page = 2
+    driver.get(url=url)
 
-            for __ in range(pod_all_page - 1):
+    for c in cookie:
+        driver.add_cookie({'name': c.split('=')[0].strip(), 'value': c.split('=')[1].strip()})
+    # Хотел через pickle записать cookies, но при работе в окне от webdriver никак не могла засчитаться капча.
+    # Надо сначала делать запрос, а потом вешать cookies
 
-                pod_all_links = driver.find_elements(by='xpath', value='//div[@data-marker="item"]')
+    all_pages = number_max_page(url=url)
+    print(time.time() - start, 'Узнал кол-во страниц.')
 
-                for link2 in pod_all_links:
-                    link2.find_element(by='class name', value=f'iva-item-titleStep-pdebR').click()
-                    driver.implicitly_wait(2)
-                    driver.switch_to.window(driver.window_handles[2])
+    all_links = []
 
-                    start = time.time()
-                    search_info_apartment_house(driver=driver, flag_write_headers=flag_write_headers)
-                    print(time.time()-start)
+    for number in range(1, all_pages):
+        time.sleep(1)
+        all_links += soup_links(driver)
+        all_links += reg_pod_links(driver)
 
-                    driver.close()
-                    driver.switch_to.window(driver.window_handles[1])
-                try:
-                    driver.find_element(by='xpath', value='//span[@data-marker="pagination-button/next"]').click()
-                except:
-                    continue
-                # Это вот до чего я смог додуматься. Поиск не существующего элемента выдаёт ошибку.
-                # А это как-то надо обходить. Да и try except работает быстро, в 3.11 вообще обещают моментально.
+        all_links = list(set(all_links))
+        print(f'Страница {number} закончена')
 
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
-            # Пришлось дублировать из-за того что тут 3 вкладки должно открыться.
+        driver.find_element(by='xpath', value='//span[@data-marker="pagination-button/next"]').click()
+        # Заметил такое, что если номера менять, то много повторений, если прокликивать, то всё тип-топ.
 
-            continue
-        except:
-            # link.find_element(by='xpath', value=f'//div[@data-marker="item-user-logo"]')
-            # Передумал так фильтровать застройщиков, т.к. у avito есть своя функция выгрузки только застройщиков.
-            # Да и фильтрация по лого так себе. Оказались застройщики без лого. И много агенств с лого.
+    print(time.time() - start, f'+ собрал все ссылки. До убирания пересечения {len(all_links)}')
 
-            link.find_element(by='class name', value=f'iva-item-titleStep-pdebR').click()
-            driver.switch_to.window(driver.window_handles[1])
+    all_links = tuple(set(all_links))
 
-            start = time.time()
-            search_info_apartment_house(driver=driver, flag_write_headers=flag_write_headers)
-            print(time.time() - start)
+    with open('links.txt', 'w') as file:
+        file.writelines(link + '\n' for link in all_links)
 
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
+    print(time.time() - start, '+ записал все ссылки в текст.')
 
-        flag_write_headers = False
-        # Внутри функции не поменяешь флаг, а шапку записывать до не комильфо.
-    print(f'!!!{time.time()-alls}!!!')
+    driver.close()
+    driver.quit()
 
-    driver.find_element(by='xpath', value='//span[@data-marker="pagination-button/next"]').click()
 
-driver.close()
-driver.quit()
+def search():
+    start = time.time()
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        'user-agent=Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.headless = True
+    driver = webdriver.Chrome(
+        service=Service("/home/ivan/PycharmProjects/pythonProject/chromedriver"),
+        options=options)
+
+    cookie = 'Скопируй из браузера.'.split(';')
+
+    url = 'https://www.avito.ru/'
+
+    driver.get(url=url)
+
+    for c in cookie:
+        driver.add_cookie({'name': c.split('=')[0].strip(), 'value': c.split('=')[1].strip()})
+
+    with open('/home/ivan/PycharmProjects/pythonProject/GammaGVA/apartments.csv', 'w') as _csv:
+
+        writer = csv.writer(_csv)
+        headers = ['url', 'Продавец', 'Тип продавца', 'Статус', 'Цена', 'Цена за метр', 'Адрес', 'Телефон',
+                   'Количество комнат', 'Общая площадь', 'Площадь кухни', 'Жилая площадь', 'Этаж', 'Балкон или лоджия',
+                   'Высота потолков', 'Санузел', 'Окна', 'Отделка', 'Тип комнат', 'Название новостройки',
+                   'Корпус, строение', 'Официальный застройщик', 'Тип участия', 'Срок сдачи', 'Тип дома',
+                   'Этажей в доме', 'Пассажирский лифт', 'Грузовой лифт', 'Двор', 'Парковка', 'Способ продажи']
+        writer.writerow(headers)
+
+        with open('/home/ivan/PycharmProjects/pythonProject/GammaGVA/links.txt') as txt:
+
+            for link in txt:
+                driver.get(url=link)
+                writer.writerow(search_info_apartment_house(driver, link).values())
+
+    print(time.time() - start, 'Обработаны все ссылки.')
+
+    driver.close()
+    driver.quit()
+
+if __name__=='__main__':
+
+    # search_links()
+    search()
